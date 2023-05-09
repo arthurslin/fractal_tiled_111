d8508908bd78de175242fbfe435cd9110b0e0970
Arthur Lin <arslin@ucsc.edu>
2023-05-08 22:12:50 -0700

    change to multithread

--- include/ThreadSafeQueue.h
+++ include/ThreadSafeQueue.h
@@ -1,52 +1,82 @@
 #pragma once
 
-template <typename T> class TSQueue {
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <thread>
 
-    protected:
-    
-    // Underlying queue
-    std::queue<T> m_queue;
-  
-    // mutex for thread synchronization
-    std::mutex m_mutex;
-  
-    // Condition variable for signaling
-    std::condition_variable m_cond;
+template <typename T> class ThreadSafeQueue {
+	
+	protected:
+	
+		// the underlying queue	
+		std::queue<T> m_queue;
+		
+		// mutex for thread synchronization
+		std::mutex m_mutex;
+		
+		// Condition variable for signaling
+		std::condition_variable m_cond;
+		
+		// number of items in the queue
+		std::atomic<size_t> m_enqued_items = 0;
 
-    public:
+		// a flag to stop the queue
+		mutable std::atomic<bool> abort_flag = false;
 
-    TSQueue() {}
+	public:
+		ThreadSafeQueue() {}
 
-    void  push(T item)
-    {
-  
-        // Acquire lock.  We will wait here if mutex locked
-        std::unique_lock<std::mutex> lock(m_mutex);
-  
-        // Add item to the non-thread safe queue
-        m_queue.push(item);
-  
-        // Notify one waiting thread that something is  waiting
-        m_cond.notify_one();
-    }
+		void abort() {
+			// this can be called from another thread to stop operation
+			abort_flag.store(true);
+			m_cond.notify_all();
+		}
 
-    T pop()
-    {
-  
-        // acquire the lock
-        std::unique_lock<std::mutex> lock(m_mutex);
-  
-        // wait until queue is not empty
-        m_cond.wait(lock,
-                    [this]() { return !m_queue.empty(); });
-  
-        // retrieve item
-        T item = m_queue.front();
-        m_queue.pop();
-  
-        // return item
-        return item;
-    }
+		void push(T item)
+		{
+
+			// Acquire lock.  We will wait here if mutex locked
+			std::unique_lock<std::mutex> lock(m_mutex);
+
+			// Add item to the non-thread safe queue
+			m_queue.push(item);
+			m_enqued_items++;
+
+			// Notify one waiting thread that something is  waiting
+			m_cond.notify_one();
+        	}
+
+   	 	T pop() {
+
+			// acquire the lock
+			std::unique_lock<std::mutex> lock(m_mutex);
+
+			// wait until queue is not empty
+			m_cond.wait(lock,
+			[this]() { return (!m_queue.empty() || is_aborted()); });
+
+			if (abort_flag) {
+				std::cout << "pop() wait is aborted due to queue closure" << std::endl;
+				return nullptr;
+			}
+
+			// retrieve item
+			T item = m_queue.front();
+			m_queue.pop();
+			m_enqued_items--;
+			// return item
+			return item;
+		}
+
+		size_t size() {
+			return m_enqued_items;
+		}
+
+		bool is_aborted() {
+			return abort_flag.load();
+		}
 
 
 };
